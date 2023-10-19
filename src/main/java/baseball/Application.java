a8cd7f981470f96c11b51857ad62c34872ffc345
HyunShik <hshong0907@gmail.com>
2023-10-19 19:46:28 +0900

    feat: 사용자 입력과 정답 매칭
    사용자의 입력과 정답을 비교해 결과를 반환한다
    1. 자리 && 숫자 모두 같으면 strike
    2. 자리는 다르지만 숫자를 포함하면 ball
    3. 0 strike 0 ball이면 낫싱

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,7 +5,7 @@ import baseball.common.Service.RandomNumberService;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        RandomNumberService randomNumberService = new RandomNumberService();
-        randomNumberService.isValid("000");
+
+
     }
 }
--- src/main/java/baseball/common/Service/RandomNumberService.java
+++ src/main/java/baseball/common/Service/RandomNumberService.java
@@ -2,14 +2,19 @@ package baseball.common.Service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RandomNumberService {
 
     private List<Integer> computer;
+    private Map<String,String> cache;
 
     public RandomNumberService(){
         computer = createRandomNumber();
+        cache = new LinkedHashMap<>();
+        System.out.println(computer);
     }
 
     //임의의 3자리 숫자 생성 로직
@@ -26,8 +31,36 @@ public class RandomNumberService {
         return list;
     }
 
+    //사용자 입력과 정답 비교
+    public String isCorrect(String input){
+
+        int strike = 0;
+        int ball = 0;
+
+        //물어본 적이 있는 질문이라면 캐시에서 반환
+        if(cache.containsKey(input)) return cache.get(input);
+
+        //유효한 입력에 대해서만 결과 반환
+        if(isValid(input)){
+
+            for(int index=0;index<3;index++){
+
+                int nowNum = input.charAt(index)-48;
+                if(nowNum==computer.get(index)) strike++;
+                else if(computer.contains(nowNum)) ball++;
+            }
+            if(strike==0&&ball==0) return "낫싱";
+            StringBuilder sb = new StringBuilder();
+            if(strike>0) sb.append(strike + "스트라이크 ");
+            if(ball>0) sb.append(ball + "볼");
+            return sb.toString();
+        }
+
+        return null;
+    }
+
     //사용자 입력 유효성 검사
-    public boolean isValid(String number){
+    private boolean isValid(String number){
 
         //사용자 입력에는 0이 포함되어서는 안된다
         if(number.contains("0")) throw new IllegalArgumentException("정답에는 0이 포함될 수 없습니다");
