fc56730f0593e8e41ad126d8f6bd526d201bc006
HyunShik <hshong0907@gmail.com>
2023-10-20 17:50:18 +0900

    refactor: baseballInGame Before,After 구분
    baseballInGame 메소드를 Before, After로 구분하여 AOP를 적용했다

--- src/main/java/baseball/common/controller/CommonController.java
+++ src/main/java/baseball/common/controller/CommonController.java
@@ -25,38 +25,9 @@ public class CommonController {
         int mode = modeSelectInRecursion();
 
         //모드 별 야구 진행
-        //종료될 때까지 반복
-        /*
-        while(true){
+        baseballInGame(mode);
 
-
-
-            //정답 맞출때까지 반복
-            while(true){
-
-                System.out.print(randomNumberService.getCount() + "번째 시도입니다! 숫자를 입력해주세요 : ");
-                String answer = randomNumberService.isCorrect(Console.readLine());
-                System.out.println(answer);
-                //3 스트라이크일 경우 break
-                if(answer.equals("3스트라이크")){
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    break;
-                }
-            }
-
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
-
-            //1과 2중에서 입력할 때까지 반복
-            while(true){
-                String input = Console.readLine();
-                //1 입력 시 재시작
-                if(input.equals("1")) break;
-                else if(input.equals("2")) return;
-                else System.out.println("1과 2 중에서 입력하세요");
-            }
-
-        }
-        */
+        //종료 or 재시작
 
     }
 
@@ -69,6 +40,32 @@ public class CommonController {
     //모드 별 야구 진행
     private void baseballInGame(int mode){
 
+        if(mode==2) NormalModeInGameInRecursion();
+
+    }
+
+    //노말 모드 인게임 재귀 함수
+    private void NormalModeInGameInRecursion(){
+        beforeInGame();
+        if(!afterInGame()) NormalModeInGameInRecursion();
+        return;
+    }
+
+    //인게임 before
+    private void beforeInGame(){
+        System.out.print(randomNumberService.getCount() + "번째 시도입니다! 숫자를 입력해주세요 : ");
+    }
+
+    //인게임 after
+    private boolean afterInGame(){
+        String answer = randomNumberService.isCorrect(Console.readLine());
+        System.out.println(answer);
+        //3 스트라이크일 경우 break
+        if(answer.equals("3스트라이크")){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+        return false;
     }
 
     //모드 설정 재귀 함수
